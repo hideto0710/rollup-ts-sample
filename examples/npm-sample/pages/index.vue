72363ba35c8b07cb4f9dfe9e6c0e464c3334849e
hideto0710 <h.inamura0710@gmail.com>
2019-01-08 00:04:31 +0900

    add nuxt sample

---
+++ examples/npm-sample/pages/index.vue
@@ -0,0 +1,47 @@
+<template>
+  <section>
+    <h1 class="header">Nuxt TypeScript Starter</h1>
+    <div class="cards">
+      <Card
+        v-for="person in people"
+        :key="person.id"
+        :person="person"
+      ></Card>
+    </div>
+  </section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "nuxt-property-decorator"
+import hello from "rollup-ts-sample"
+import { State } from "vuex-class"
+import { Person } from "~/types";
+import Card from "~/components/Card.vue"
+
+@Component({
+  components: {
+    Card
+  }
+})
+export default class extends Vue {
+  @State people: Person;
+
+  mounted() {
+    hello.configure({ key: "hello" }).send();
+  }
+}
+</script>
+
+<style scoped>
+.header {
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+}
+
+.cards {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
